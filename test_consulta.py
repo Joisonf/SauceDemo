9ee06e0663da301ddb6edb40dc35c26c57940e8b
Joisonf <joisonfs@gmail.com>
2024-07-13 10:18:47 -0300

    Verificações da pagina de inventario

--- test_consulta.py
+++ test_consulta.py
@@ -9,7 +9,6 @@ class TestConsultaprodutoselecionado():
   # 2.1 Atributos
   url = "https://www.saucedemo.com"
 
-
   # 2.2 Funções e metodos
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
@@ -21,5 +20,12 @@ class TestConsultaprodutoselecionado():
   
   def test_Consulta(self):
     self.driver.get(self.url)
-    self.driver.find_element(By.ID,"user-name").send_keys("standard_user")
-    self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")
+    self.driver.find_element(By.ID,"user-name").send_keys("standard_user")   #escreve o usuario
+    self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")   #escreve a senha
+    self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()     #clique no botão login
+
+    #transição da pagina
+
+    assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"
+    assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"
+    assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
